760b03f1935fcb2ddae995571bb42ccfc4163b47
Kathryn Hackman <kathrynhackman@Kathryns-MacBook-Pro-2.local>
2024-12-30 21:21:47 -0500

    resolve deployment error

--- app/lib/placeholder-data.ts
+++ app/lib/placeholder-data.ts
@@ -3,6 +3,7 @@
 const users = [
     {
       id: '410544b2-4001-4271-9855-fec4b6a6442a',
+      user_id: '123',
       name: 'Test User',
       email: 'user@nextmail.com',
       password: '123456',
